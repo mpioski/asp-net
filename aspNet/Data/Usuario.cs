7861d3d4899db00d0e4dfaa127cc53c5a443cc18
mpioski <5nbhwd7240963>
2018-10-04 21:11:53 -0300

    Start

---
+++ aspNet/Data/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspNet.Data
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
---
+++ aspNet/Pages/Usuarios/Cadastrar.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aspNet.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspNet.Pages.Usuarios
+{
+    public class CadastrarModel : PageModel
+    {
+        [BindProperty]
+        public Usuario usuario { get; set; }
+        public void OnGet(String nome, String senha)
+        {
+            if (usuario == null)
+                usuario = new Usuario();
+
+            usuario.Nome = nome;
+            usuario.Senha = senha;
+        }
+    }
+}
